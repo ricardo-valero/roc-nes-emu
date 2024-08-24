8becb821e01d3cb0ae8d47cd87ee8691681b936a
Ricardo Valero de la Rosa <55701657+ricardo-valero@users.noreply.github.com>
2024-08-24 10:40:36 -0600

    repo setup

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    roc = {
+      url = "github:roc-lang/roc";
+      # inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    nixpkgs,
+    roc,
+    ...
+  }: let
+    systems = nixpkgs.lib.systems.flakeExposed;
+  in {
+    formatter = nixpkgs.lib.genAttrs systems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        pkgs.alejandra
+    );
+    devShells = nixpkgs.lib.genAttrs systems (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      rocPkgs = roc.packages.${system};
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nil
+          (with rocPkgs; [full])
+        ];
+      };
+    });
+  };
+}
